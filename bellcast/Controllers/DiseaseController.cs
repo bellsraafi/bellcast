8aa006d27e6295b29da550069793639099cab5de
Bello Raafi <bellsraafi@gmail.com>
2018-12-13 11:03:09 +0100

    Added alot of files

---
+++ bellcast/Controllers/DiseaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bellcast.Controllers
+{
+    public class DiseaseController : Controller
+    {
+        // GET: Disease
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ bellcast/Models/List.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bellcast.Models
+{
+    public class List
+    {
+        public int dt { get; set; }
+        public Temp temp { get; set; }
+        public double pressure { get; set; }
+        public int humidity { get; set; }
+        public List<Weather> weather { get; set; }
+        public double speed { get; set; }
+        public int deg { get; set; }
+        public int clouds { get; set; }
+    }
+}
